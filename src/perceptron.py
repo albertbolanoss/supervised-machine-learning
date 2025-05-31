ba01313f28ed807f9f1945e11c5f297c4fa23287
Luis Alberto Bolaños Salazar <68343185+albertbolanoss@users.noreply.github.com>
2025-05-31 14:10:47 -0500

    Perceptron exercises

---
+++ src/perceptron.py
@@ -0,0 +1,53 @@
+
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+def score(weights, bias, features):
+    return features.dot(weights) + bias
+
+def step(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
+def prediction(weights, bias, features):
+    return step(score(weights, bias, features))
+
+def error(weights, bias, features, label):
+    pred = prediction(weights, bias, features)
+    if pred == label:
+        return 0
+    else:
+        return np.abs(score(weights, bias, features))
+
+def mean_perceptron_error(weights, bias, features, labels):
+    total_error = 0
+    for i in range(len(features)):
+        total_error += error(weights, bias, features[i], labels[i])
+    return total_error/len(features)
+
+def perceptron_trick(weights, bias, features, label, learning_rate = 0.01):
+    pred = prediction(weights, bias, features)
+    for i in range(len(weights)):
+        weights[i] += (label-pred)*features[i]*learning_rate
+    bias += (label-pred)*learning_rate
+    return weights, bias
+
+def perceptron_algorithm(features, labels, learning_rate = 0.01, epochs = 200):
+    random.seed(0)
+    weights = [1.0 for i in range(len(features[0]))]
+    bias = 0.0
+    errors = []
+    for epoch in range(epochs):
+    
+        error = mean_perceptron_error(weights, bias, features, labels)
+        errors.append(error)
+        i = random.randint(0, len(features)-1)
+        weights, bias = perceptron_trick(weights, bias, features[i], labels[i])
+    
+    plt.scatter(range(epochs), errors)
+    plt.show()
+
+    return weights, bias
